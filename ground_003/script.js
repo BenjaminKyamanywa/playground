a6cacebf500aea74b0d5f6c25d08cd35b1e151c6
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-01-17 00:02:55 +0300

    refactor automatic footer year update

--- ground_003/script.js
+++ ground_003/script.js
@@ -1,9 +1,14 @@
 // when the DOM is ready
-document.addEventListener("DOMContentLoaded", function() {
-  // get the span element
-  const yearSpan = document.querySelector('.footer-date');
-  // get the current year
-  const currentYear = new Date().getFullYear();
-  // set the span element's text to current year
-  yearSpan.textContent = currentYear;
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   // get the span element
+//   const yearSpan = document.querySelector('.footer-date');
+//   // get the current year
+//   const currentYear = new Date().getFullYear();
+//   // set the span element's text to current year
+//   yearSpan.textContent = currentYear;
+// });
+
+// get span element
+const year = document.querySelector('.footer-date');
+// set span element's text to current year
+year.innerHTML = new Date().getFullYear();
