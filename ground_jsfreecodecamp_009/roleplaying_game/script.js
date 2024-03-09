427cdcea34be2b6942463964da1abfa8967940b3
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-10 01:49:48 +0300

    add initial styles UI card

--- ground_jsfreecodecamp_009/roleplaying_game/script.js
+++ ground_jsfreecodecamp_009/roleplaying_game/script.js
@@ -8,4 +8,6 @@ let monsterHealth;
 let inventory = ["stick"];
 
 // UI variable initializations
-let button1 = document.querySelector("#button1");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
