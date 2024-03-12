e0fc2f954714045def3729edaba398a8127d2bd2
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-12 04:04:27 +0300

    add fighting function with logic

--- ground_jsfcc_009/roleplaying_game/script.js
+++ ground_jsfcc_009/roleplaying_game/script.js
@@ -36,6 +36,23 @@ const weapons = [
     power: 100
   }
 ];
+const monsters = [
+  {
+    name: "slime",
+    level: 2,
+    health: 15,
+  },
+    {
+    name: "fanged beast",
+    level: 8,
+    health: 60,
+  },
+    {
+    name: "dragon",
+    level: 20,
+    health: 300,
+  }
+];
 const locations = [
   {
     name: "town square",
@@ -54,6 +71,12 @@ const locations = [
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ];
 
@@ -129,14 +152,32 @@ function sellWeapon () {
 }
 
 function fightSlime () {
-
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast () {
-
+  fighting = 1;
+  goFight();
 }
 
 function fightDragon () {
-  console.log("Fighting Dragon.");
+  fighting = 2;
+  goFight();
 }
 
+function goFight () {
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
+
+function attack () {
+
+}
+
+function dodge () {
+
+}
