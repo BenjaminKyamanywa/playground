cd0e5ca0004180833a90393bbecd91aa62453588
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-18 15:08:12 +0300

    add health update from monsters leverl

--- ground_jsfcc_009/roleplaying_game/script.js
+++ ground_jsfcc_009/roleplaying_game/script.js
@@ -177,6 +177,7 @@ function goFight () {
 function attack () {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level - health;
 }
 
 function dodge () {
