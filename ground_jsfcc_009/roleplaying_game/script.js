3067c25e5a197a3a7349e2d474280c40576c9b47
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-20 11:48:51 +0300

    add player success message if the number they choose is right

--- ground_jsfcc_009/roleplaying_game/script.js
+++ ground_jsfcc_009/roleplaying_game/script.js
@@ -287,5 +287,13 @@ function pick (guess) {
  while (numbers.length < 10) {
   numbers.push(Math.floor(Math.random() * 11));
  }
- text.innerText = "You picked " + guess + ". Here are the random numbers:";
+ text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+ for (let i = o; i < 10; i++) {
+  text.innerText += numbers[i] + "\n";
+ }
+ if (numbers.includes(guess)) {
+  text.innerText += "Right! You win 20 gold!";
+  gold += 20;
+  goldText.innerText = gold;
+ }
 }
