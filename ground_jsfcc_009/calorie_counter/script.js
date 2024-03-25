fdd40522f3b3ae05e8f3791faca7e05ebf8033c9
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-25 23:01:00 +0300

    add string replace for our regex with empty space

--- ground_jsfcc_009/calorie_counter/script.js
+++ ground_jsfcc_009/calorie_counter/script.js
@@ -11,6 +11,11 @@ let isError = false;
 
 // clean string value received from input
 function cleanInputString (str) {
+
+  /*
+  
+  While looping through the string works, creating a new array is inefficient for memory and runtime performance. Instead, we'll use Regular Expressions (regex)to match specific characters.
+  
   const strArray = str.split('');
   const cleanStrArray = [];
   // iterate through each character in strArray
@@ -19,4 +24,10 @@ function cleanInputString (str) {
       cleanStrArray.push(strArray[i]);
     }
   }
+
+  */
+
+  // utilize regex - more performant
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
 }
