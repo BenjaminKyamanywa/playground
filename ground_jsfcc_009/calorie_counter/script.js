d9fd3245abcdca672b5d4566f1224f60fd6bff42
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-25 14:39:32 +0300

    split string within cleanInputString function and create new variable to store valid number characters

--- ground_jsfcc_009/calorie_counter/script.js
+++ ground_jsfcc_009/calorie_counter/script.js
@@ -5,3 +5,13 @@ const entryDropdown = document.getElementById('entry-dropdown');
 const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
+
+// set boolean variables
+let isError = false;
+
+
+// function to clean string value received from input
+function cleanInputString (str) {
+  const strArray = str.split('');
+  const cleanStrArray = [];
+}
