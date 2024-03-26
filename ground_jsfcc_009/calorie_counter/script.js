e4d90dbaa12759a120a680b140756508f3e65da5
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-27 02:24:53 +0300

    add template literals to get html inputs

--- ground_jsfcc_009/calorie_counter/script.js
+++ ground_jsfcc_009/calorie_counter/script.js
@@ -24,6 +24,13 @@ function isInvalidInput (str) {
 
 // allow users to add calorie counter entries
 function addEntry () {
-  const targetId = "#" + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <input type="number" min="0" id="${entryDropdown.value}-${entryNumber}-calories" placeholder="Calories" />
+  `;
+  targetInputContainer.innerHTML += HTMLString;
 }
