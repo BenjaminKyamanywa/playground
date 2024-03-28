65474c9661a93a97eeb0d4974358cde7813760f7
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-03-28 17:10:07 +0300

    change text to lower case for output element

--- ground_jsfcc_009/calorie_counter/script.js
+++ ground_jsfcc_009/calorie_counter/script.js
@@ -51,6 +51,17 @@ function calculateCalories (e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+  if (isError) {
+    return
+  }
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}"></span>`;
 }
 
 // get calorie counts from user's entries
