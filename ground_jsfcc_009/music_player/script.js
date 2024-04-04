0cc5bcb58d19f1baa8b6e28f7bc68f95b408f6d6
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-05 01:51:18 +0300

    add player playlist playButton and pauseButton variables

---
+++ ground_jsfcc_009/music_player/script.js
@@ -0,0 +1,5 @@
+// assigning variable playlist elements 
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+
