883ab01d96918e055f107284bdc14a79036187a1
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-10 05:48:03 +0300

    add completion for play next song function

--- ground_jsfcc_009/music_player/script.js
+++ ground_jsfcc_009/music_player/script.js
@@ -117,12 +117,18 @@ const pauseSong = () => {
 
 // play next son
 const playNextSong = () => {
-
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex(); 
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
+  }
 }
 
 // play previous song
 const playPreviousSong = () => {
-  
+
 }
 
 // display songs on the UI
