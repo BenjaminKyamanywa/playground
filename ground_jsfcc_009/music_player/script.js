e3072e18cb71311bf0e33c23db77c97cb2581ca1
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-11 15:05:39 +0300

    create button with js and add id and aria label

--- ground_jsfcc_009/music_player/script.js
+++ ground_jsfcc_009/music_player/script.js
@@ -168,6 +168,13 @@ const deleteSong = (id) => {
   renderSongs(userData?.songs);
   highlightCurrentSong();
   setPlayButtonAccessibleText();
+
+  if (userData?.songs.length === 0) {
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+  }
 }
 
 // set current playing song onto music player display
