c1c8995ff426b11474c7646aa5587e93960cb427
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-11 14:45:05 +0300

    add check for when deleting a song

--- ground_jsfcc_009/music_player/script.js
+++ ground_jsfcc_009/music_player/script.js
@@ -155,6 +155,14 @@ const shuffle = () => {
 
 // music player delete song functionality
 const deleteSong = (id) => {
+
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong();
+    setPlayerDisplay();
+  }
+
   userData.songs = userData?.songs.filter((song) => song.id !== id);
   
   renderSongs(userData?.songs);
