564ad1efd1dd7dac4f608a8889c4226796434d22
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-16 13:15:07 +0300

    add fix for uncaught type error of date not showing on the UI

--- ground_jsfcc_009/date_formatter/script.js
+++ ground_jsfcc_009/date_formatter/script.js
@@ -1,13 +1,15 @@
 // initialize variables
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
+
 const date = new Date();
 const day = date.getDate();
 const month = date.getMonth() + 1;
 const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
-const formattedDate =  `${day}-${month}-${year}`;
+
+const formattedDate = `${day}-${month}-${year}`;
 
 // utilizing variables
 currentDateParagraph.textContent = formattedDate;
