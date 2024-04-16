477c9880d5d458424b39b51b067929d3629377f3
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-16 08:08:37 +0300

    add event listener date options select

--- ground_jsfcc_009/date_formatter/script.js
+++ ground_jsfcc_009/date_formatter/script.js
@@ -3,3 +3,19 @@ const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
 const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const formattedDate =  `${day}-${month}-${year}`;
+
+// utilizing variables
+currentDateParagraph.textContent = formattedDate;
+
+// event listeners
+dateOptionsSelectElement.addEventListener("change", () => {
+  // return user selected date
+  switch (dateOptionsSelectElement.value) {
+    
+  }
+});
