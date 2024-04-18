21932eb6b46ee3b2a1ff7f5e739833eb4f19d56a
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-18 15:25:56 +0300

    add empty string input checker with alert for user

--- ground_jsfcc_009/palindrom_checker/script.js
+++ ground_jsfcc_009/palindrom_checker/script.js
@@ -2,3 +2,17 @@
 const userInput = document.getElementById("text-input");
 const submitButton = document.getElementById("check-btn");
 const resultContainer = document.getElementById("result");
+
+const checkForPalindrome = (input) => {
+  const initialInput = input;
+
+  if (input === "") {
+    alert("Please input a value");
+    return
+  }
+
+  resultContainer.replaceChildren();
+
+  
+  
+}
