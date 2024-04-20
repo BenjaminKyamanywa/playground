3ad20a99daea9417eb3b3715fa6203756f5e4b6d
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-21 01:32:52 +0300

    add default for switch statement for call of player cards

--- ground_jsfcc_009/football_team_cards/script.js
+++ ground_jsfcc_009/football_team_cards/script.js
@@ -221,6 +221,11 @@ playersDropdownList.addEventListener("change", (e) => {
     case "defender":
       setPlayerCards(players.filter((player) => player.position === "defender"));
       break;
+    case "goalkeeper":
+      setPlayerCards(players.filter((player) => player.position === "goalkeeper"));
+      break;
+    default:
+        setPlayerCards();
   }
 
 });
