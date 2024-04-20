cfd74a07862501f16cf93e5fd5cfd55b1baa590a
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-21 01:30:32 +0300

    add case of if player is a defender

--- ground_jsfcc_009/football_team_cards/script.js
+++ ground_jsfcc_009/football_team_cards/script.js
@@ -218,6 +218,9 @@ playersDropdownList.addEventListener("change", (e) => {
     case "midfielder":
       setPlayerCards(players.filter((player) => player.position === "midfielder"));
       break;
+    case "defender":
+      setPlayerCards(players.filter((player) => player.position === "defender"));
+      break;
   }
 
 });
