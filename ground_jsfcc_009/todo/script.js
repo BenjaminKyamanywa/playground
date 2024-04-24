843c004543f773864635a9c6956cf4a6352afd69
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-24 11:03:56 +0300

    add get items from localstorage

--- ground_jsfcc_009/todo/script.js
+++ ground_jsfcc_009/todo/script.js
@@ -11,7 +11,8 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = []
+const taskData =  JSON.parse(localStorage.getItem("data")) || [];
+
 let currentTask = {}
 
 // add or update task
