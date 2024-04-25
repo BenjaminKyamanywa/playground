3f68d205c9595ccd80b474463b55600a3141e7b4
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-26 02:40:25 +0300

    add event listeners for click and keydown on user convert button submission

--- ground_jsfcc_009/decimal_to_binary/script.js
+++ ground_jsfcc_009/decimal_to_binary/script.js
@@ -2,3 +2,14 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+// check input value
+const checkUserInput = () => {
+
+}
+
+// user convert btn
+convertBtn.addEventListener("click", checkUserInput);
+
+// listen for keyboard enter and return keys
+numberInput.addEventListener("keydown", () => {})
