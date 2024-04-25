77fcf6ad21a69d138a323a978f681690b607f5c6
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-26 02:27:45 +0300

    initialize app variables

--- ground_jsfcc_009/decimal_to_binary/script.js
+++ ground_jsfcc_009/decimal_to_binary/script.js
@@ -1,1 +1,4 @@
 // Initiate variables
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
