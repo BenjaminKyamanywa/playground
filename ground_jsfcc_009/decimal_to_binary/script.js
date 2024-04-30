a72666aace4e6a103b68e2bc0f4011ffae3f3b1f
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-04-30 16:51:19 +0300

    add recursive logic to decimalToBinary function

--- ground_jsfcc_009/decimal_to_binary/script.js
+++ ground_jsfcc_009/decimal_to_binary/script.js
@@ -5,30 +5,23 @@ const result = document.getElementById("result");
 
 // decimal to binary conversion
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
-}
+};
 
 // check input value
 const checkUserInput = () => {
   if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
     alert("Please provide a decimal number");
-    return
+    return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
-}
+};
 
 // user convert btn
 convertBtn.addEventListener("click", checkUserInput);
