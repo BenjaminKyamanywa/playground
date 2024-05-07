40b7cdf0217120a615326f8636805397d6dd387c
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-05-07 12:29:45 +0300

    add initializations for variable inputs

---
+++ ground_jsfcc_009/roman_converter/script.js
@@ -0,0 +1,5 @@
+// initialize variables from inputs
+const form = document.getElementById("form");
+const buttonConvert = document.getElementById("convert-btn");
+const output = document.getElementById("output");
+
