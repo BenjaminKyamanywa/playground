20d0ddfe4e5bdcc1f8eeafc8beebb0a23df879cd
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-05-10 01:26:51 +0300

    add check message event listener for empty input alert

--- ground_jsfcc_009/spam_filter/script.js
+++ ground_jsfcc_009/spam_filter/script.js
@@ -1,2 +1,22 @@
 // initialize variables
+const messageInput = document.getElementById("message-input");
+const result = document.getElementById("result");
+const checkMessageButton = document.getElementById("check-message-btn");
 
+// regex variables
+const helpRegex = /please help/i;
+
+
+
+// event listeners
+checkMessageButton.addEventListener("click", () => {
+  if (messageInput.value === "") {
+    alert("Please enter a message.");
+    return;
+  }
+
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
+  messageInput.value = "";
+});
