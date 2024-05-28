61b14241f8d9bfdded308e5827cf04ff321d6ab8
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-05-29 00:45:50 +0300

    feat: add sortButton event listener

--- ground_jsfcc_009/number_sorter/script.js
+++ ground_jsfcc_009/number_sorter/script.js
@@ -6,3 +6,6 @@ const sortButton = document.getElementById("sort");
 const sortInputArray = (event) => {
   event.preventDefault();
 }
+
+// event listeners
+sortButton.addEventListener("click", sortInputArray);
