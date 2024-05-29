e3d6663e15e93797573680067842bd4749327727
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-05-29 13:42:50 +0300

    feat: update bubble sort function

--- ground_jsfcc_009/number_sorter/script.js
+++ ground_jsfcc_009/number_sorter/script.js
@@ -23,7 +23,21 @@ const updateUI = (array = []) => {
 }
 
 // initialize bubbleSort
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      console.log(array, array[j], array[j + 1]);
 
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
 
 // event listeners
 sortButton.addEventListener("click", sortInputArray);
