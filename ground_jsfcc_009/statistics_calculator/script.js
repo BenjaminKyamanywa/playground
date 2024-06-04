de951a60f5281597c42775902ee4085ae40e2bde
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-04 23:55:08 +0300

    feat: add calculate variables for filering out numbers from user input

--- ground_jsfcc_009/statistics_calculator/script.js
+++ ground_jsfcc_009/statistics_calculator/script.js
@@ -3,4 +3,7 @@
 // calculate function
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => Number(el));
+  const filtered = numbers.filter();
 }
