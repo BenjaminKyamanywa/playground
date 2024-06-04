851cd172bcf2dcae2013bab95114dabfbc618e21
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-05 00:14:36 +0300

    feat: add getMean function

--- ground_jsfcc_009/statistics_calculator/script.js
+++ ground_jsfcc_009/statistics_calculator/script.js
@@ -1,9 +1,13 @@
 // initialize variables
 
+// getMean function
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
 // calculate function
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 
+  const mean = getMean(numbers);
 }
