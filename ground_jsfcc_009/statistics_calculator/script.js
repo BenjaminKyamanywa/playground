2d59545b1ec7edf7a40aafb74c8d52aa35a7dcd3
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-06 00:50:06 +0300

    feat: add getMedian function and utilize it in calculate function

--- ground_jsfcc_009/statistics_calculator/script.js
+++ ground_jsfcc_009/statistics_calculator/script.js
@@ -3,6 +3,17 @@
 // getMean function
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
+// getMedia function
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+}
+
+
 // calculate function
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -10,7 +21,11 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
+
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 
 }
+
