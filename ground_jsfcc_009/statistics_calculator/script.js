1b5f4b857ad67ffb0adfe5cbfb3d2b98a1bcaa8f
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-06 02:00:34 +0300

    feat: add getRange function

--- ground_jsfcc_009/statistics_calculator/script.js
+++ ground_jsfcc_009/statistics_calculator/script.js
@@ -5,7 +5,7 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 // getMedia function
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.slice().sort((a, b) => a - b);
   const median =
     array.length % 2 === 0
       ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
@@ -29,6 +29,11 @@ const getMode = (array) => {
     (el) => counts[el] === counts[highest]
   );
   return mode.join(", ");
+}
+
+// getRange function
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
 }
 
 // calculate function
