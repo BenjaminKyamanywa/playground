5d3fd835d462c5b8f3e934b2b2746a6403272e45
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-13 07:41:28 +0300

    feat: add range function for number generation

--- ground_jsfcc_009/spreadsheet/script.js
+++ ground_jsfcc_009/spreadsheet/script.js
@@ -1,3 +1,7 @@
+
+// generate a range of numbers
+const range = (start, end) => Array();
+
 // call browser window onload funtion
 window.onload = () => {
   const container = document.getElementById("container");
