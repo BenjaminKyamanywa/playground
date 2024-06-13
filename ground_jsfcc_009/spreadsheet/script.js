b0763bb98a38ae4cacff3c89f31e017275337e64
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-13 07:38:21 +0300

    feat: add class plus textContent and appendChild as label element

--- ground_jsfcc_009/spreadsheet/script.js
+++ ground_jsfcc_009/spreadsheet/script.js
@@ -3,5 +3,8 @@ window.onload = () => {
   const container = document.getElementById("container");
   const createLabel = () => {
     const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
   }
 }
