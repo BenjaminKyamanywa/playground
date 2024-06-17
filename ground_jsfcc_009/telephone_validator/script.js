bca38181611c60344f83ec77d1004eddfc98ffbd
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-17 07:08:35 +0300

    feat: add check for valid input function

--- ground_jsfcc_009/telephone_validator/script.js
+++ ground_jsfcc_009/telephone_validator/script.js
@@ -4,3 +4,33 @@ const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const results = document.getElementById("results-div");
 
+// check for valid number
+const checkForValidNumber = input => {
+  if (input === "") {
+    alert("Please provide a phone number");
+    return;
+  }
+
+  const countryCode = '^(1\\s?)?';
+  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spacesDashes = '[\\s\\-]?';
+  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+  const phoneRegex = new RegExp(
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  );
+
+  const paragraphElement = document.createElement("p");
+  paragraphElement.className = "results-text";
+  phoneRegex.test(input)
+    ? (paragraphElement.style.color = "#00471b")
+    : (paragraphElement.style.color = "#4d3800");
+  paragraphElement.appendChild(
+    document.createTextNode(
+      `
+      ${phoneRegex.test(input) ? 'Valid' 
+      : 'Invalid'} US number: ${input}
+      `
+    )
+  );
+  results.appendChild(paragraphElement);
+};
