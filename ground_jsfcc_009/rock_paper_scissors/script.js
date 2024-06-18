4fb80ab3fb8ef0548fd1fd25c5df815de5efd5fb
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-18 15:28:18 +0300

    feat: add function check for winner between user and computer

--- ground_jsfcc_009/rock_paper_scissors/script.js
+++ ground_jsfcc_009/rock_paper_scissors/script.js
@@ -14,3 +14,20 @@ const hasPlayerWonTheRound = (player, computer) => {
   );
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+// get results of the round between user and computer
+const getRoundResults = (userOption) => {
+  const computerResult = getRandomComputerResult();
+
+  if(hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
