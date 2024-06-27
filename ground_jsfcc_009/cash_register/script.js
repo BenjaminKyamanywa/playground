e482a98e41367c8e8461f406264f73c930753644
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-06-27 17:12:28 +0300

    feat: add changeDueDisplay variable update

--- ground_jsfcc_009/cash_register/script.js
+++ ground_jsfcc_009/cash_register/script.js
@@ -12,7 +12,7 @@ let cid = [
   ["ONE HUNDRED", 100]
 ];
 
-const changeDue = document.getElementById("change-due");
+const changeDueDisplay = document.getElementById("change-due");
 const cash = document.getElementById("cash");
 const purchaseBtn = document.getElementById("purchase-btn");
 const priceScreen = document.getElementById("price-screen");
@@ -20,9 +20,26 @@ const cashDrawer = document.getElementById("cash-drawer-display");
 
 // format results
 const formatResults = (status, change) => {
-  changeDue.innerHTML `<p>Status: ${status}</p>`;
+  changeDueDisplay.innerHTML `<p>Status: ${status}</p>`;
   change.map(
-    money => (changeDue.innerHTML += `<p>${money[0]}: $${money[1]}</p>`)
+    money => (changeDueDisplay.innerHTML += `<p>${money[0]}: $${money[1]}</p>`)
   );
   return
+}
+
+// check cash register funds
+const checkCashRegister = () => {
+  if (Number(cash.value) < price) {
+    alert('Customer does not have enough money to purchase the item');
+    cash.value = "";
+    return
+  }
+
+  if (Number(cash.value) === price) {
+    changeDueDisplay.innerHTML = '<p>No change due - customer paid with exact cash</p>';
+    cash.value = "";
+    return
+  }
+
+  
 }
