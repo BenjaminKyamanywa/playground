ddf2669e62619f7e7e50ce07b763d07c4044dcf1
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-07-16 00:29:44 +0300

    feat: fix transition out for drumkit

--- ground_011/01 - Drum Kit/script.js
+++ ground_011/01 - Drum Kit/script.js
@@ -1,26 +1,29 @@
+
+// play sound 
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  // stop function from running all together
+  if(!audio) return;
+
+  key.classList.add('playing');
+  audio.currenTime = 0; //rewind audio to the start
+  audio.play();
+  // add playing class
+}
+
 // remove transiton
 const removeTransition = (e) => {
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
 }
 
-// play sound 
-const playSound = (e) => {
-  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keycode}"]`);
-  // stop function from running all together
-  if(!audio) return;
-  // add playing class
-  key.classList.add('playing');
-  audio.currenTime = 0; //rewind audio to the start
-  audio.play();
-}
 
 // select all keys
 const keys = Array.from(document.querySelectorAll('.key'));
 
 // event listener for each key to fire remove transition
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 // window object event listener to playSound
 window.addEventListener('keydown', playSound);
