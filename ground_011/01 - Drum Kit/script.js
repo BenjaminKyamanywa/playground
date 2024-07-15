3510f92be0907e825d7405d5375313506e6df258
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-07-16 01:57:59 +0300

    feat: add initializer function for playing sound on click

--- ground_011/01 - Drum Kit/script.js
+++ ground_011/01 - Drum Kit/script.js
@@ -3,7 +3,7 @@
 const playSound = (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  // stop function from running all together
+  // if pressed key has no audio element stop function execution
   if(!audio) return;
 
   key.classList.add('playing');
@@ -18,12 +18,20 @@ const removeTransition = (e) => {
   e.target.classList.remove('playing');
 }
 
+// play sound onclick
+const handleClick = (e) => {
+//  add code for handling click
+
+}
 
 // select all keys
 const keys = Array.from(document.querySelectorAll('.key'));
+const items = Array.from(document.querySelectorAll('.key'));
 
 // event listener for each key to fire remove transition
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+items.forEach(key => key.addEventListener('click', handleClick));
+
 
 // window object event listener to playSound
 window.addEventListener('keydown', playSound);
