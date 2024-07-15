2dcd757cfef3bc3b5c0f0e9a7c1e8397add096db
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-07-15 22:49:03 +0300

    feat: add initialize for playSound function

--- ground_011/01 - Drum Kit/script.js
+++ ground_011/01 - Drum Kit/script.js
@@ -1,1 +1,16 @@
-// Initialize variables
+
+// play sound 
+const playSound = (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
+  const key = document.querySelector(`div[data-key"${e.keycode}"]`);
+  // stop function from running all together
+  if(!audio) return;
+  key.classList.add('playing');
+  audio.currenTime = 0; //rewind audio to the start
+  audio.play();
+
+}
+
+
+
+// remove transiton
