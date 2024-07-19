4c36ae55660cc1dcd39209b9a550988a8696e518
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-07-19 20:00:15 +0300

    feat: initialize secondHand movement

--- ground_011/02 - Clock/script.js
+++ ground_011/02 - Clock/script.js
@@ -1,1 +1,17 @@
 // Initialize variables
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+// set the date of clock
+const setDate = () => {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+}
+
+setInterval(setDate, 1000);
+
+setDate();
