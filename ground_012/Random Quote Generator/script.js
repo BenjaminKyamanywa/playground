e1f93a0f05b222e1a489eed943d61ec77cc537fa
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-08-06 23:43:04 +0300

    feat: add free jacinto api call url as comment

--- ground_012/Random Quote Generator/script.js
+++ ground_012/Random Quote Generator/script.js
@@ -1,6 +1,6 @@
 // Get quotes from API
 const getQuotes = async = async () => {
-  // add URL for our API call
+  // add URL for our API call 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
   const apiURL = 'https://type.fit/api/quotes';
 
   try {
