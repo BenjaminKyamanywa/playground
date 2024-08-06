5ef294c7d8fec201aaabd006e485f635567e3729
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-08-06 22:35:02 +0300

    feat: add api utilization with fetch call for random the quotes

---
+++ ground_012/Random Quote Generator/script.js
@@ -0,0 +1,12 @@
+// Get quotes from API
+const getQuotes = async = async () => {
+  // add URL for our API call
+  const apiURL = 'https://type.fit/api/quotes';
+
+  try {
+    const response = await fetch(apiURL);
+
+  } catch (error) {
+    // Catch error 
+  }
+}
