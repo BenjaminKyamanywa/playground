eb7dfcf5a96da9005d4c4c667a3b4d5521aef061
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-08-07 23:38:27 +0300

    feat: initilize element variables and clean up markup hard coded quote and author text

--- ground_012/Random Quote Generator/script.js
+++ ground_012/Random Quote Generator/script.js
@@ -1,3 +1,12 @@
+// initialize element variables
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
 let apiQuotes = [];
 
 // show new Quote 
