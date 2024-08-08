63df6e9c546e27262ae6b0c5a3607b3ea43dca4c
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-08-08 13:11:05 +0300

    feat: add calls for showing loader and complete loader when loading quotes

--- ground_012/Random Quote Generator/script.js
+++ ground_012/Random Quote Generator/script.js
@@ -11,12 +11,19 @@ let apiQuotes = [];
 // show loading
 const loading = () => {
   loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-// 
+// hide loading
+const completeLoading = () => {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 // show new Quote 
 const newQuote = () => {
+  // show loader
+  loader();
   // pick random quote from quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -34,11 +41,15 @@ const newQuote = () => {
     quoteText.classList.remove('long-quote');
   }
 
+  // set quote, hide loader
   quoteText.textContent = quote.text;
+  completeLoading();
 }
 
 // Get quotes from API
 const getQuotes = async = async () => {
+  // show loader
+  loader();
   // add URL for our API call 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
   const apiURL = 'https://type.fit/api/quotes';
 
