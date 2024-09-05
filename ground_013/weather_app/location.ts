10f617294c28a47a633f80cf8e9b87ec5a95be99
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-09-05 19:08:20 +0300

    feat: add fetch for api

--- ground_013/weather_app/location.ts
+++ ground_013/weather_app/location.ts
@@ -6,6 +6,25 @@ export interface LocationInfo {
   display_name: string;
 }
 
-export async function fetchLocationData(apiURL: string, location: string) {
-  
+export async function fetchLocationData(apiURL: string, location: string): Promise<LocationInfo> {
+  const options = {
+    method: "GET",
+    url: apiURL,
+    params: {
+        q: location,
+        api_key: process.env.GEOCODE_API_KEY
+    },
+  };
+
+  const response = await axios.request<LocationInfo[]>(options);
+
+  if (response.status === 200) {
+    if (response.data.length > 0) {
+      return response.data[0];
+    } else {
+      throw new Error(`Unable to find location information for ${location}`);
+    }
+  } else {
+    throw new Error("Failed to fetch location data");
+  }
 }
