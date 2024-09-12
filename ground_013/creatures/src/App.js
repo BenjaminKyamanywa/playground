9297917ae686e06837c88729f54b9cb8960b4873
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-09-12 15:34:14 +0300

    chore: add creatures react project dependencies

--- ground_013/creatures/src/App.js
+++ ground_013/creatures/src/App.js
@@ -1,25 +1,46 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // constructor method for our state
+  constructor() {
+    super();
+
+    // instantiate state
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  // component mount initialization
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+
+  }
 }
 
 export default App;
