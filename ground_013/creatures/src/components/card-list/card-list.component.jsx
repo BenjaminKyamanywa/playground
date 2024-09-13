b961aea7f02355877e05e823d5adc41c676a2d7b
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-09-13 08:18:54 +0300

    feat: add optimization with new card component and styels

--- ground_013/creatures/src/components/card-list/card-list.component.jsx
+++ ground_013/creatures/src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Card } from '../card/card.component'
 import './card-list.styles.css'
 
 class CardList extends Component {
@@ -10,18 +11,8 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {creatures.map((creature) => {
-          
-          const { name, id, email } = creature; 
-
           return (
-            <div className='card-container' key={id}>
-              <img 
-              src={`https://robohash.org/${id}/?set=set4&size=180x180`} 
-              alt={`creature ${name}`} 
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
+            <Card creature={creature} />
         )})}
       </div>
     )
---
+++ ground_013/creatures/src/components/card/card.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+import './card-styles.css';
+
+class Card extends Component {
+
+  render() {
+
+    const { name, email, id } = this.props.creature;
+
+    return(
+      <div className='card-container' key={id}>
+      <img 
+      src={`https://robohash.org/${id}/?set=set4&size=180x180`} 
+      alt={`creature ${name}`} 
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+    )
+  }
+
+}
+
+export default Card;
