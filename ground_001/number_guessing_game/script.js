3bb1aee1d430859035d27f067ad03b6750c8b929
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-09-18 18:58:39 +0300

    chore: add guessCheck logic for when a player chooses a number

--- ground_001/number_guessing_game/script.js
+++ ground_001/number_guessing_game/script.js
@@ -19,5 +19,35 @@ function checkGuess() {
   // get user value and ensure it's a number
   const userGuess = Number(guessField.value);
 
-  
+  // check if it's players first go at the game
+  if (guessCount === 1) {
+    // make guesses paragraph text content equal to previous guesses
+    guesses.textContent = 'Previous guesses:';
+  }
+
+  // append current user user guess value onto the end of guesses paragraph with blank space inbetween
+  guesses.textContent = `${guesses.textContent} ${userGuess}`;
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right! 🤩'
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '🤒 !!!Game Over!!! 🤒';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong';
+    lastResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
 }
