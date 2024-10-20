eeef3fb24f502973c3e0b78e2979e6410541e77a
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-10-20 20:59:32 +0300

    feat: add initial element variables and function for story app

--- ground_001/story_generator/script.js
+++ ground_001/story_generator/script.js
@@ -1,1 +1,9 @@
 // initialize variables
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){ // takes an array and returns one of the items stored in it randomly
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
