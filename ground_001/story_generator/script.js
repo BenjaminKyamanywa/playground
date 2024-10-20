511e23c90e234b57213342b29926a5c540845007
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-10-20 21:03:09 +0300

    feat: add app input text strings

--- ground_001/story_generator/script.js
+++ ground_001/story_generator/script.js
@@ -1,4 +1,4 @@
-// initialize variables
+// initialize app variables
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -7,3 +7,9 @@ function randomValueFromArray(array){ // takes an array and returns one of the i
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+// raw text strings as inputs for our app
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy','Father Christmas',];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
