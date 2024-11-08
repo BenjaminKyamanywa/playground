997dcd8a7c73e0e36981bcaae27ad30ff7a3ae8e
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2024-11-08 23:06:27 +0300

    chore: add boiler script calendar

--- ground_001/calendar/script.js
+++ ground_001/calendar/script.js
@@ -2,3 +2,27 @@
 const select = document.querySelector('select');
 const list = document.querySelector('ul');
 const heading = document.querySelector('h1');
+
+function createCalendar(month) {
+  let days = 31;
+
+  // TODO: ADD CONDITIONAL HERE
+
+  list.textContent = '';
+  heading.textContent = month;
+
+  for (let i = 1; i <= days; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = i;
+    list.appendChild(listItem);
+  }
+};
+
+select.value = 'January';
+createCalendar('January');
+
+// select click event listener
+select.addEventListener('change', () => {
+  const choice = select.value;
+  createCalender(choice);
+});
