824a768e63bec9e946b410a449a3d1c91628d4ef
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2025-05-19 08:30:00 +0300

    chore: add page theme function

--- ground_003/Cake Recipe Page/script.js
+++ ground_003/Cake Recipe Page/script.js
@@ -1,1 +1,18 @@
-// Cake Recipe Page Javascript
+// Cake Recipe Page Javascript setTheme
+
+const setTheme = (theme) => {
+  let root = document.documentElement;
+  if (theme === 'light') {
+    root.style.setProperty('--bg-color', '#ECF2FF');
+    root.style.setProperty('--text-color', '#2B283A');
+    root.style.setProperty('--title-color', '#4A4E74');
+  } else if (theme === 'dark') {
+    root.style.setProperty('--bg-color', '#2B283A');
+    root.style.setProperty('--text-color', '#ECF2FF');
+    root.style.setProperty('--title-color', '#D5D4D8');
+
+  }
+}
+
+//invoke setTheme function
+setTheme('light');
