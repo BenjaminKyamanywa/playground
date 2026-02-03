491a7f9d63825e1d4db367ff98fcd0c211beb262
Benjamin Kyamanywa <benjaminkyam@gmail.com>
2026-02-03 08:30:00 +0300

    chore: add initial templating for playGame function

--- ground_001/guess_the_number/script.js
+++ ground_001/guess_the_number/script.js
@@ -2,7 +2,7 @@
 Action Items list for Guess The Number Game
 TODO; GET User value from input and save it to variable numberGuess
 TODO: Generate a random number between 1 to 100 and save it to variable correctNumber
-TODO: Console whether the guess is too high, too low, or is correct
+TODO: Console whether the guess is too high, too low, or is correct inside playGame function
 TODO: Create a function called displayResult to move the 
 TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
 */
@@ -10,6 +10,7 @@ TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
 // Variable to store list of guesses
 
 // Variable to store the correct random number
+let correctNumber = getRandomNumber();
 
 // window.onload() Function: Waits for entire html page to load and enables us perform functions
 window.onload = function () {
@@ -22,6 +23,15 @@ function playGame() {
 
   // Get and save the submitted user value in a variable
   let numberGuess = document.getElementById("number-guess").value;
+
+  // Check whether the guess is too high, too low or correct
+  if (numberGuess > correctNumber) {
+    console.log("too high");
+  } else if (numberGuess < correctNumber) {
+    console.log("too low");
+  } else {
+    console.log("is correct")
+  }
 }
 
 /*
@@ -32,5 +42,26 @@ Show result if guess is too high, too low, or correct
 Initialize a new game by resetting all values and content on the app
 */
 function initGame() {
-  
+
+}
+
+/*
+Reset HTML content for guess history
+*/
+function resetResultContent() {
+  document.getElementById("result").innerHTML = "";
+}
+
+/*
+Return a random number between 1 and 100
+*/
+function getRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+/*
+Save guess history
+*/
+function saveGuessHistory(guess) {
+
 }
